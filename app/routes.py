be0a4723f756bc2bcc58add813dc6c3a1241ca5e
Archit Kashyap <archit.kashyap@iiitb.org>
2018-12-17 21:13:44 +0530

    Template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Archit'}
+    posts = [
+        {
+            'author': {'username': 'Kashyap'},
+            'body': 'Back to college'
+        },
+        {
+            'author': {'username': 'Shubham'},
+            'body': 'Trying to learn web development'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
